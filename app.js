5ca2b373157a619f54ef4617efc5a1762e42d0f0
Carlos Solano <carlos_andrey8@hotmail.com>
2013-10-08 18:23:11 -0600

    Manejo de error de conexión a Mongo

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ var mongoose = require('mongoose');
 //Para conectarse a la base de datos indicada en config.db
 mongoose.connect(config.db);
 
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error de conexión:'));
+db.once('open', function callback () {
+  console.log('Conexión a Mongo abierta');
+});
+
 require('./models/roles');
 var dbRol = mongoose.model('Rol'); //Con esto tenemos el modelo rol listo para ser guardado en mongo
 
