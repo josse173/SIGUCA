c093fb04c12ff90865ab6bc9dfa660f4970a1bfb
honguitoxic <tatimb14@gmail.com>
2013-10-21 00:39:04 -0600

    Modificación filtro empleados en graficos

---
+++ public/javascripts/graficos.js
@@ -0,0 +1,29 @@
+function changeImageAlex(){
+	
+    document.getElementById("grafico").src= '/images/alex.PNG';
+    cambiaTextoB();
+
+    }
+function cambiaTextoB(){
+	document.getElementById("empleadosSelec").text('Alexander Picado');
+}
+function changeImageCar(){
+	
+    document.getElementById("grafico").src= '/images/Carlos.PNG';
+
+    }
+ function changeImageTa(){
+	
+    document.getElementById("grafico").src= '/images/tati.PNG';
+
+    }
+ function changeImageGa(){
+	
+    document.getElementById("grafico").src= '/images/gabriel.PNG';
+
+    }
+ function changeImageRi(){
+	
+    document.getElementById("grafico").src= '/images/rita.PNG';
+
+    }
