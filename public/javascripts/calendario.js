1455ce1d0de68f5b893df9f0b4f767f551b5ba4f
Gabriel Martínez Barboza <gabriel.mb20@gmail.com>
2013-11-04 12:54:15 -0600

    Modificaciones Calendario Empleado.

--- public/javascripts/calendario.js
+++ public/javascripts/calendario.js
@@ -6,22 +6,29 @@ $(document).ready(function (){
             data: { message: 'Llegada tardía por accidente de tránsito.' }
         },
         {
-            date: new Date("October 14, 2013 03:24:00"),
+            date: new Date("November 04, 2013 03:24:00"),
             data: { message: 'Permiso personal.' }
         },
         {
-            date: new Date("October 18, 2013 03:24:00"),
-            data: { message: 'Llegada tardía por accidente de tránsito.' }
+            date: new Date("November 19, 2013 03:24:00"),
+            data: { message: 'Vacaciones' }
         },
+        {
+            date: new Date("November 22, 2013 03:24:00"),
+            data: { message: 'Horas Extras Aprobadas' }
+        },
+        {
+            date: new Date("November 28, 2013 03:24:00"),
+            data: { message: 'Cita Médica' }
+        },        
     ],
     onClick: function(target, cell, date, data) {
         target.val(date.getYear() + ' - ' +
                     date.getMonth() + ' - ' +
                     date.getDate());
         if(data != null) {
-             alert('Justificación de ausencia/tardía\n' + 'Empleado: Gabriel Martínez Barboza (123456789)\n' + 'Detalle: ' + data.message + '\n' + 'Fecha:' + date.toDateString() + ' ' + date.toTimeString());
-        }
-        window.confirm("¿Acepta la justificación?");
+             alert('Permisos-Ausencias\n' + 'Empleado: Gabriel Martínez Barboza (123456789)\n' + 'Detalle: ' + data.message + '\n' + 'Fecha:' + date.toDateString() + ' ' + date.toTimeString());
+        }        
     }
     });
 })
