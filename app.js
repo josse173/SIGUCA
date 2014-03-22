90bfdf52b49ae51fcd6c2283b48980827128e1ff
Carlos Solano <carlos_andrey8@hotmail.com>
2014-03-21 21:27:47 -0600

    Modificar las configuraciones de Passport a app.js y ordenar el despiche que había. Donde hay orden...!

--- app.js
+++ app.js
@@ -1,75 +1,95 @@
-/** SIGUCA (Sistema de Gestión de Usuarios y Control de Asistencia)
- * Aplicación principal- 
-   Main app
- * 		
+/** 
+ *
+ * 	SIGUCA (Sistema de Gestión de Usuarios y Control de Asistencia)
+ * 	Aplicación principal	
+ *
+ */
+ 
+/** 
+ *
+ *	Dependencias
+ *
+ */
+ 
+var path = require('path');
+var express = require('express');
+var http = require('http');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var routes = require('./routes');
+//var apiEmpleado = require('../controllers');
+
+/**
+ *
+ *	Configuración principal
+ *
+ */
+ 
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    app.use(express.errorHandler());
+});
+
+/**
+ *
+ *	Configuración de Passport
  *
  */
 
+var Usuario = require('./models/usuario');
+passport.use(new LocalStrategy(Usuario.authenticate()));
+passport.serializeUser(Usuario.serializeUser());
+passport.deserializeUser(Usuario.deserializeUser());
 
-/** Dependencias
-    Dependencies
+/**
+ *
+ *	Mongoose
+ *
  */
-var express = require('express');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var routes = require('./routes');
-var path = require('path');
-LocalStrategy = require('passport-local').Strategy;
-//var apiEmpleado = require('../controllers');
 
-//crea o conecta a base de datos 
 mongoose.connect('mongodb://localhost/sigucadb',function(err){
 	if(!err){
-		console.log('conectado a sigucadb');
+		console.log('Conectado a SIGUCADB');
 	}else{
 	 throw err;
 	}
 });
 
-var app = express();
-
-/*Configuración de ambientes.s
-* Configuration */
-app.configure(function(){
-	app.set('port', process.env.PORT || 3000);
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
-	app.use(express.logger('dev'));
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(app.router);
-	app.use(express.static(path.join(__dirname, 'public')));
-
-});
-
 /** Leer la configuración de ./config/config **/
 var env = process.env.NODE_ENV || 'development';
 var config = require('./config/config')[env];
-
-
-
-// Configure passport
-var Account = require('./models/cuentas-admin');
-
-passport.use(new LocalStrategy(Account.authenticate()));
-passport.serializeUser(Account.serializeUser());
-passport.deserializeUser(Account.deserializeUser());
-
-// development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
   app.locals.pretty = true;
-  //app.configure('development', function () { app.locals.pretty = true; });
 }
 
-
-/** Routes
-**/
+/**
+ *
+ *	Rutas
+ *
+ */
+ 
 require('./routes/index')(app);
 
-// Open App socket
-app.listen(3000);
-console.log('Servidor Express escuchando en http://localhost:' + app.get('port'));
-
-
+app.listen(app.get('port'), function(){
+  console.log(("Servidor escuchando en puerto: " + app.get('port')))
+});
