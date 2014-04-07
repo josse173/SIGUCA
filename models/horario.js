0cc361baad49fa1f6563fd839a414d8e11ff40aa
honguitoxic <tatimb14@gmail.com>
2014-04-06 23:41:47 -0600

    Se crea un modelo de horario

---
+++ models/horario.js
@@ -0,0 +1,18 @@
+  /** SIGUCA 
+ *
+ * 	Modelo de Horario
+ *  Schedule model
+ */
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var SchemaHorario = new Schema({
+		idSupervisor: {type : Schema.ObjectId},
+		nombre: { type: String, default: '' },
+		horaEntrada: { type: String, default: '8:00' }, 
+		horaSalida: { type: String, default: '17:00'},
+		horaInAlmuerzo: {type: String, default: '12:00'},
+		horaFnAlmuerzo: {type: String, default: '13:00'},
+		rangoReceso: {type: String, default: '45m'}
+}
