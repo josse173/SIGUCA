da6151f1a624858659f17064e2326d3ae589bae9
Alexander Picado <alexpj17@gmail.com>
2014-05-10 17:30:20 -0600

    Insertar Date Picker y CRUD

--- models/Horario.js
+++ models/Horario.js
@@ -1,20 +1,38 @@
   /** SIGUCA 
- *
- * 	Modelo de Horario
- *  Schedule model
- */
+   *
+   *Modelo de Horario
+   *  Schedule model
+   */
 
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+  var mongoose = require('mongoose'),
+      Schema = mongoose.Schema;
 
-var SchemaHorario = new Schema({
-		
-		nombre: { type: String, default: '' },
-		horaEntrada: { type: String, default: '8:00' }, 
-		horaSalida: { type: String, default: '17:00'},
-		horaInAlmuerzo: {type: String, default: '12:00'},
-		horaFnAlmuerzo: {type: String, default: '13:00'},
-		rangoReceso: {type: String, default: '45m'}
-});
+  var SchemaHorario = new Schema({
 
-module.exports = mongoose.model('Horario', SchemaHorario);
+      nombre: {
+          type: String,
+          default: ''
+      },
+      horaEntrada: {
+          type: String,
+          default: '8:00'
+      },
+      horaSalida: {
+          type: String,
+          default: '17:00'
+      },
+      horaInAlmuerzo: {
+          type: String,
+          default: '12:00'
+      },
+      horaFnAlmuerzo: {
+          type: String,
+          default: '13:00'
+      },
+      rangoReceso: {
+          type: String,
+          default: '45m'
+      }
+  });
+
+  module.exports = mongoose.model('Horario', SchemaHorario);
