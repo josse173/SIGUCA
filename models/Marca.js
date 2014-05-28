5ed107fdab5704def8c9e13bc3de6c4a90e92be4
Gabriel Martínez Barboza <gabriel.mb20@gmail.com>
2014-05-28 10:41:22 -0600

    Corrección en modelo Marca.

--- models/Marca.js
+++ models/Marca.js
@@ -3,56 +3,22 @@
  *  Modelo de Marca
  *
  */
-
-
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     passportLocalMongoose = require('passport-local-mongoose');
 //Crear el esquema de Marca
 var SchemaMarca = new Schema({
-<<<<<<< HEAD
-    fecha: ({
-      dia: { type: Number, default: 0 },
-      mes: { type: Number, default: 0 },
-      ano: { type: Number, default: 0 }
-    }), 
-    horaMarca: ({
-      hora: { type: Number, default: 0 },
-      minutos: { type: Number, default: 0 },
-      segundos: { type: Number, default: 0 }
-    }),
-    codTarjeta: { type: String, default: 0 },
-    // horaEntrada: ({
+    // fecha: ({
+    //   dia: { type: Number, default: 0 },
+    //   mes: { type: Number, default: 0 },
+    //   ano: { type: Number, default: 0 }
+    // }), 
+    // horaMarca: ({
     //   hora: { type: Number, default: 0 },
     //   minutos: { type: Number, default: 0 },
     //   segundos: { type: Number, default: 0 }
     // }),
-    // horaSalida: ({
-    //   hora: { type: Number, default: 0 },
-    //   minutos: { type: Number, default: 0 },
-    //   segundos: { type: Number, default: 0 }
-    // }),
-    // horaSalidaReceso: ({
-    //   hora: { type: Number, default: 0 },
-    //   minutos: { type: Number, default: 0 },
-    //   segundos: { type: Number, default: 0 }
-    // }),
-    // horaEntradaReceso: ({
-    //   hora: { type: Number, default: 0 },
-    //   minutos: { type: Number, default: 0 },
-    //   segundos: { type: Number, default: 0 }
-    // }),
-    // horaSalidaAlmuerzo: ({
-    //   hora: { type: Number, default: 0 },
-    //   minutos: { type: Number, default: 0 },
-    //   segundos: { type: Number, default: 0 }
-    // }),
-    // horaEntradaAlmuerzo: ({
-    //   hora: { type: Number, default: 0 },
-    //   minutos: { type: Number, default: 0 },
-    //   segundos: { type: Number, default: 0 }
-    // }),    
-=======
+    // codTarjeta: { type: String, default: 0 },   
     dia: {
         type: Number,
         default: 0
@@ -90,8 +56,6 @@ var SchemaMarca = new Schema({
         type: Number,
         default: 0
     },
-
->>>>>>> dc5e6a90856937ab285c1a9cdeb079e3a88aab55
 });
 
 SchemaMarca.plugin(passportLocalMongoose);
