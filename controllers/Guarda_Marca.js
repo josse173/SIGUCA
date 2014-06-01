cc7ca4db769efb72425818d8fe03b331e0f3fecf
Gabriel Martínez Barboza <gabriel.mb20@gmail.com>
2014-05-31 19:00:58 -0600

    Marca ingresada a la BD :D

--- controllers/Guarda_Marca.js
+++ controllers/Guarda_Marca.js
@@ -1,8 +1,5 @@
-/** SIGUCA  
-* Registro de Marca en la Base de Datos
-*
-**/
-
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/sigucadb');
 var Marca = require('../models/Marca.js');
 // var sleep = require("sleep");
 var serialport = require("serialport");
@@ -16,54 +13,28 @@ serialPort.open(function () {
   console.log('Puerto Serial /dev/ttyO1 Abierto.');
   var subcod;  
   var codigo;
-//  var tiempo = new Date();
-//  var current = getDateTime();
-//  console.log(current);
+  var d = new Date();
+  var nuevaMarca; 
   serialPort.on("data", function(data) {
     //sleep.sleep(5); // 5 segundos
     subcod = data.substr(5,6); // Toma desde la posición 5 del código, 6 elementos: 590040155458 -> 401554
     subcod = parseInt(subcod,16); // Convierte el código de Hexadecimal a Decimal:  401554 -> 4199764
     console.log('Codigo: 000'  + subcod); // Se imprime el código anteponiendo 3 ceros al inicio para que se muestre igual que en el llavero
-//    guarda(subcod);
-  }); 
-});
+  
+  var marca = new Marca({ 
+     tipoMarca: "Entrada",
+     dia: (d.getUTCDate() - 1),
+     mes: (d.getMonth() + 1),
+     ano: d.getFullYear(),
+     hora: d.getHours(),
+     minutos: d.getMinutes(),
+     segundos: d.getSeconds(),
+     codTarjeta: subcod,
+  });
+  marca.save(function (err) {
+    if (err) // ...
+    console.log('test');
+  });
 
-// Guarda nueva marca
-//exports.guarda = function(codigo) {
-// function guarda(codigo) {
-//	var date = new Date();
-//	var marca = Marca({    
-//			dia: ({
-//		        date.getUTCDate()    
-//		    }),
-//		    mes: ({
-//		        date.getMonth()
-//		    }),
-//		    ano: ({
-//		        date.getFullYear()
-//		    }),
-//		    hora: ({
-//		        date.getHours()
-//		    }),
-//		    minutos: ({
-//		        date.getMinutes()
-//		    }),
-//		    segundos: ({
-//		        date.getSeconds()
-//		    }),
-//		    tipoMarca: ({
-//				type: String,
-//				default: "Entrada" //Entrada-salida-salidaReceso-EntradaReceso-salidaAlmuerzo-entradaAlmuerzo
-//			}),
-//			estado: ({
-//				type: String,
-//				default: "Normal" //Normal-Omision-Tardia
-//			}),
-//			codTarjeta: ({ 
-//				codigo
-//			})
-//	});				 
-//	marca.save(function (err) {
-//		if (err) throw err;
-//	});				
-//}
+  }); //  serialPort.on("data", function(data) 
+}); // serialPort.open(function () 
