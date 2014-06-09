acdd18591d1c8bfb8ccebc25e38328d27272a08c
SIGUCA BeagleBone Black <grupo02siguca@gmail.com>
2014-06-08 22:46:36 -0600

    Cambios controlador Marca.

--- controllers/Guarda_Marca.js
+++ controllers/Guarda_Marca.js
@@ -1,40 +1,49 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/sigucadb');
 var Marca = require('../models/Marca.js');
-// var sleep = require("sleep");
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
 var serialPort = new SerialPort("/dev/ttyO1", {
   baudrate: 9600,
   parser: serialport.parsers.readline("\03", "ascii")
-}, false); 
+}, false);
+
+// function sleep(time, callback) {
+//     var stop = new Date().getTime();
+//     while(new Date().getTime() < stop + time) {
+//         ;
+//     }
+//     callback();
+// }
 
 serialPort.open(function () {
   console.log('Puerto Serial /dev/ttyO1 Abierto.');
-  var subcod;  
-  var codigo;
-  var d = new Date();
-  var nuevaMarca; 
-  serialPort.on("data", function(data) {
-    //sleep.sleep(5); // 5 segundos
-    subcod = data.substr(5,6); // Toma desde la posición 5 del código, 6 elementos: 590040155458 -> 401554
+}); // serialPort.open(function () 
+
+
+serialPort.on("data", function (data) {
+    // Se enciende el puerto serial
+    console.log("Puerto Serial Activado");
+
+    // Fecha actual en formato EPOC-UNIX
+    var fecha = new Date();
+    var epochTime = (fecha.getTime() - fecha.getMilliseconds())/1000;
+
+    // Lectura y conversión del código
+    var subcod = data.substr(5,6); // Toma desde la posición 5 del código, 6 elementos: 590040155458 -> 401554
     subcod = parseInt(subcod,16); // Convierte el código de Hexadecimal a Decimal:  401554 -> 4199764
     console.log('Codigo: 000'  + subcod); // Se imprime el código anteponiendo 3 ceros al inicio para que se muestre igual que en el llavero
-  
-  var marca = new Marca({ 
-     tipoMarca: "Entrada",
-     dia: (d.getUTCDate() - 1),
-     mes: (d.getMonth() + 1),
-     ano: d.getFullYear(),
-     hora: d.getHours(),
-     minutos: d.getMinutes(),
-     segundos: d.getSeconds(),
-     codTarjeta: subcod,
-  });
-  marca.save(function (err) {
-    if (err) // ...
-    console.log('test');
-  });
+    // Crea el objeto con la nueva para marca
+    var marca = new Marca({
+       tipoMarca: "Entrada",
+       epoch: epochTime,
+       codTarjeta: ("000" + subcod),
+    });
+   // Guarda la marca en la Base de Datos
+   console.log("Guardando la marca en la BD...");
+   marca.save(function (err) {
+     if (err)
+       console.log('Error al guardar la marca en la Base de Datos.');
+   }); // marca.save
+}); //  serialPort.on("data", function(data) 
 
-  }); //  serialPort.on("data", function(data) 
-}); // serialPort.open(function () 
