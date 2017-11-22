62fa1b336e939fa9004329019e6e9a2e80a541a1
GustavoNajera <tavinchi.com@gmail.com>
2017-11-22 17:49:04 -0600

    Correos al cumplir 12 días de vacaciones y aumento automatico cada mes

--- routes/crudUsuario.js
+++ routes/crudUsuario.js
@@ -484,3 +484,133 @@ exports.getEmpleadoPorSupervisor = function(idSupervisor, usuarioQuery, callback
 		});//
 	});//
 }
+
+exports.updateVacaciones = function(){
+	//Configuración al correo
+	var transporter = nodemailer.createTransport('smtps://'+config.emailUser+':'+config.emailPass+'@'+config.emailEmail);
+	var emailSIGUCA 	= 'siguca@greencore.co.cr';
+	var fechaActual = new Date();
+
+	//Obtiene usuarios activos
+	Usuario.find({estado: "Activo", fechaIngreso: {$exists: true, $ne:0}, tipo: {$ne:"Supervisor"}}).populate("departamentos").exec(function(err, listUserTem){
+		
+		/**
+		 * Validaciones para optener los usuarios a los que se les debe aumentar vacaciones
+		 */
+		Usuario.find({estado:"Activo", tipo:"Supervisor"}).populate("departamentos").exec(function(err, listSupervisor){
+			
+			var listIdUser = [];
+			var listUser = [];
+			for (var i = 0, len = listUserTem.length; i < len; i++) {
+				var user = listUserTem[i];
+
+				//Valida que no tome en cuenta usuarios que solo sean profesores
+				if(user.tipo.length != 1 || user.tipo != "Profesor" ){
+
+					//Valida que el día sea igual al actual
+					var fechaIngreso = new Date(user.fechaIngreso*1000);
+					if(fechaIngreso.getDate() == fechaActual.getDate()){
+						listIdUser.push(user.id);
+						listUser.push(user);
+					}
+				}
+			}
+
+			/**
+			 * Aumento de vacaciones
+			 */
+			Usuario.update({_id:{$in: listIdUser}}, {$inc:{vacaciones:1}},{multi:true}, function(err){
+
+				/**
+				 * Envío de correos
+				 */
+				var listEmail = [];
+				for (var cont = 0; cont < listUser.length; cont++) {
+					var user = listUser[cont];
+					if(user.vacaciones >= 11){//11 porque La lista utilizada no tiene el aumento de vacaciones
+												
+						/**
+						 * Envío de correo a empleados.
+						 */
+						transporter.sendMail({
+							from: emailSIGUCA,
+							to: user.email,
+							subject: 'Acumulación de vacaciones',
+							text: " Señor " + user.nombre + " " + user.apellido1 
+							+ ", usted dispone de " + (user.vacaciones+1) + " días de vacaciones, se recomienda no exceder 12 días,"
+							+ " favor contactar a su supervisor.\n\n ¡Saludos!" 
+						});
+
+						//Recorre departamentos del usuario
+						for (var index = 0; index < user.departamentos.length; index++) {
+							var dpt = user.departamentos[index];
+							
+							//Recorre supervisores
+							for (var insts = 0; insts < listSupervisor.length; insts++) {
+								var spv = listSupervisor[insts];
+
+								//Recorre los dpts de cada supervisor
+								for (var instd = 0; instd < spv.departamentos.length; instd++) {
+									var dptSpv = spv.departamentos[instd];
+									
+									//Verifica el departamento del usuario con el departamento del supervisor
+									if((dpt.departamento+"") == (dptSpv.departamento+"")){
+
+										//Verifica en la lista de correos a enviar.
+										var existe = false;
+										for (var instE = 0; instE < listEmail.length; instE++) {
+											var email = listEmail[instE];
+
+											if(email.idSpv && email.idSpv == spv.id){
+												existe = true;
+
+												//Busca en los id concatenados para no repetirlos de nuevo
+												var buscaUsr = false;
+												for (var instIdEmail = 0; instIdEmail < email.idUser.length; instIdEmail++) {
+													var emailUser = email.idUser[instIdEmail];
+													if(emailUser == user.id){
+														buscaUsr = true;
+													}
+												}
+												if(!buscaUsr){
+													listEmail[instE].message += "\t" + user.nombre + " " + user.apellido1 + " ("+(user.vacaciones+1)+")\n";
+													listEmail[instE].idUser.push(user.id);
+												}
+											}
+										}
+										//Si no existe el supervisor en el listado de correos, se agrega
+										if(!existe){
+											listEmail.push({
+												nameSpv: spv.nombre + " " + spv.apellido1,
+												idSpv: spv.id,
+												message: "\t" + user.nombre + " " + user.apellido1 + " ("+(user.vacaciones+1)+")\n ",												idUser: [user.id],
+												emailSpv: spv.email,
+												idUser: [user.id]
+											});
+										}
+									}
+								}
+							}
+						}
+					}
+				}			
+				
+				
+				/**
+				 * Envía correos a los supervisores
+				 */
+				for (var instIdEmail = 0; instIdEmail < listEmail.length; instIdEmail++) {
+					var email = listEmail[instIdEmail];
+					transporter.sendMail({
+						from: emailSIGUCA,
+						to: email.emailSpv,
+						subject: 'Acumulación de vacaciones',
+						text: "Estimado(a) " + email.nameSpv +", se le informa que la siguiente lista de usuarios ha superado 11 días de vacaciones acumulados:\n" 
+						+ email.message + "\n\n ¡Saludos!"
+					});
+					
+				}
+			});
+		});
+	});//Fin consulta obtiene usuarios
+}
