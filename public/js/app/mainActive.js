ec3abb7bfb0cb8e52371fccb07762b798d03915b
GustavoNajera <tavinchi.com@gmail.com>
2017-11-24 08:53:36 -0600

    Se modifica el menú y se arregla bug en reportes

--- public/js/app/mainActive.js
+++ public/js/app/mainActive.js
@@ -3,16 +3,16 @@ window.onload = function(){
         var listElement = document.getElementsByClassName("activeControl");
         for(var cont = 0; cont < listElement.length; cont ++){
             if(listElement[cont].getAttribute("href") == window.location.pathname){
-                listElement[cont].classList.add("btn-primary");
-                listElement[cont].style.color = "#ffffff";
+                listElement[cont].classList.add("activeT");
+                //listElement[cont].style.color = "#ffffff";
 
                 //Valida si el elemento activado sea hija, en caso de serlo se activa el padre
                 var size = listElement[cont].classList.length;
                 for(var cont2 = 0; cont2 < size; cont2++){
                     var result = listElement[cont].classList.item(cont2).split("-");
                     if(result.length > 1 && result[0] == "son"){
-                        document.getElementsByClassName("father-"+result[1])[0].classList.add("btn-primary");
-                        document.getElementsByClassName("father-"+result[1])[0].style.color = "#ffffff";
+                        document.getElementsByClassName("father-"+result[1])[0].classList.add("activeT");
+                        //document.getElementsByClassName("father-"+result[1])[0].style.color = "#ffffff";
                     }
                 }
             }
