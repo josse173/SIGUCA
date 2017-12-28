9cf4bed705344ecadec4c19c771b99f887a2770a
tony950819 <tony950819@gmail.com>
2017-12-28 10:10:50 -0600

    Bug envio de correo al eliminar justificacion

--- routes/crudJustificaciones.js
+++ routes/crudJustificaciones.js
@@ -268,7 +268,7 @@ exports.deleteJust = function(id, cb){
 		if(just.fechaCreada)
 			fecha = moment(just.fechaCreada);
 		Correo.find({},function(errorCritico,listaCorreos){
-			if(!errorCritico &&listaCorreos>0){
+			if(!errorCritico &&listaCorreos.length>0){
 				var transporter = nodemailer.createTransport('smtps://'+listaCorreos[0].nombreCorreo+':'+listaCorreos[0].password+'@'+listaCorreos[0].dominioCorreo);
 					transporter.sendMail({
 						from: listaCorreos[0].nombreCorreo,
@@ -280,6 +280,8 @@ exports.deleteJust = function(id, cb){
 						+ " \r\n Motivo: " + just.motivo
 						+ " \r\n Detalle: " + just.detalle
 					});
+			}else{
+				console.log("error al enviar correo de eliminado de justificación");
 			}
 		});
 
