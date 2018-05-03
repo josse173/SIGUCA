1fbd1850dd76cf5b28e6cee67d725399f70f7084
GustavoNajera <tavinchi.com@gmail.com>
2018-05-03 08:43:17 -0600

    Se resuelve problema con asignación del tipo de usuario al realizar marca desde el fingerprint

--- routes/index.js
+++ routes/index.js
@@ -488,7 +488,7 @@ module.exports = function(app, io) {
             var codTarjeta = req.param('codTarjeta');
             var tipoMarca = req.param('tipoMarca');
             var tipoUsuario = req.param('tipo');
-            globalTipoUsuario = tipoUsuario;
+            global.globalTipoUsuario = tipoUsuario;
             var ip = req.param('ipv4');
             //if(pwd1 == 'ooKa6ieC' && pwd2 == 'of2Oobai' ) {
             
