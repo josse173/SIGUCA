30ad2186325425f774a644fdc865fdaa6f917f79
chrislorig <chrislorig26@gmail.com>
2019-01-10 12:24:30 -0600

    RF016- Mejora en la interfaz del reloj marcador en la PI

--- siguca-rfid/Subprocess_BackEnd.py
+++ siguca-rfid/Subprocess_BackEnd.py
@@ -1,28 +1,27 @@
 #coding=utf-8
 #/**
- #  GreenCore Solutions
- #* Python-Extension to SIGUCA application
- #*
- #* Copyright 2016 by Bradley Hidalgo Guzman <bfhg.17@hotmail.com>
- #*
- #* This file is part of SIGUCA open source application.
- #* 
- #* SIGUCA open source application is free software: you can redistribute 
- #* it and/or modify it under the terms of the GNU Affero General Public 
- #* License as published by the Free Software Foundation, either 
- #* version 3 of the License, or (at your option) any later version.
- #* 
- #* SIGUCA open source application is distributed in the hope that it will 
- #* be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
- #* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- #* GNU Affero General Public License for more details.
- #* 
- #* You should have received a copy of the GNU Affero General Public License
- #* along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
- #*
- #* 
- #*/
-
+#  GreenCore Solutions
+#* Python-Extension to SIGUCA application
+#*
+#* Copyright 2016 by Bradley Hidalgo Guzman <bfhg.17@hotmail.com>
+#*
+#* This file is part of SIGUCA open source application.
+#*
+#* SIGUCA open source application is free software: you can redistribute
+#* it and/or modify it under the terms of the GNU Affero General Public
+#* License as published by the Free Software Foundation, either
+#* version 3 of the License, or (at your option) any later version.
+#*
+#* SIGUCA open source application is distributed in the hope that it will
+#* be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+#* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#* GNU Affero General Public License for more details.
+#*
+#* You should have received a copy of the GNU Affero General Public License
+#* along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#*
+#*
+#*/
 import urllib
 from bson.objectid import ObjectId
 import time
@@ -39,12 +38,11 @@ from keyboard_alike import reader
 #Import para imagenes
 import UtilImg
 
-
 #SETTINGS AND CONFIGURATIONS
 #IP OF NODE JS SERVER WHERE SIGUCA IS RUNNING
 server_IP='siguca.greencore.int'
 #server_IP='192.168.1.4'
-#PORT OF THE MONGODB 
+#PORT OF THE MONGODB
 port='27017'
 #PORT OF OF SIGUCA NODE JS PORT
 app_Port='3000'
@@ -67,7 +65,6 @@ def getIpv4():
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     s.connect(('<broadcast>', 0))
     return s.getsockname()[0]
-
 
 #Methods to define the timestamps thought the web browser
 def Entrada(dec,tipo):
@@ -106,6 +103,7 @@ def SalidaReceso(dec,tipo):
         pass
     root3.after(4000, lambda: root3.destroy())
     root3.mainloop()
+
 def EntradaReceso(dec,tipo):
     #browser = subprocess.Popen([browserSelection, 'http://'+server_IP+':'+app_Port+'/rfidReader?pwd1=ooKa6ieC&pwd2=of2Oobai&codTarjeta='+dec+'&tipoMarca=3'])
     #time.sleep(1)
@@ -128,11 +126,12 @@ def EntradaReceso(dec,tipo):
         pass
     root3.after(4000, lambda: root3.destroy())
     root3.mainloop()
+
 def SalidaAlmuerzo(dec,tipo):
-   # browser = subprocess.Popen([browserSelection, 'http://'+server_IP+':'+app_Port+'/rfidReader?pwd1=ooKa6ieC&pwd2=of2Oobai&codTarjeta='+dec+'&tipoMarca=4'])
-   # time.sleep(1)
-   # browser.terminate()
-   # root.destroy()
+    # browser = subprocess.Popen([browserSelection, 'http://'+server_IP+':'+app_Port+'/rfidReader?pwd1=ooKa6ieC&pwd2=of2Oobai&codTarjeta='+dec+'&tipoMarca=4'])
+    # time.sleep(1)
+    # browser.terminate()
+    # root.destroy()
     root3 = Tk()
     root3.attributes('-fullscreen', True)
     frame3 = Frame(root3)
@@ -150,6 +149,7 @@ def SalidaAlmuerzo(dec,tipo):
         pass
     root3.after(4000, lambda: root3.destroy())
     root3.mainloop()
+
 def EntradaAlmuerzo(dec,tipo):
     #browser = subprocess.Popen([browserSelection, 'http://'+server_IP+':'+app_Port+'/rfidReader?pwd1=ooKa6ieC&pwd2=of2Oobai&codTarjeta='+dec+'&tipoMarca=5'])
     #time.sleep(1)
@@ -172,6 +172,7 @@ def EntradaAlmuerzo(dec,tipo):
         pass
     root3.after(4000, lambda: root3.destroy())
     root3.mainloop()
+
 def Salida(dec,tipo):
     #browser = subprocess.Popen([browserSelection, 'http://'+server_IP+':'+app_Port+'/rfidReader?pwd1=ooKa6ieC&pwd2=of2Oobai&codTarjeta='+dec+'&tipoMarca=6'])
     #time.sleep(1)
@@ -195,7 +196,7 @@ def Salida(dec,tipo):
     root3.after(4000, lambda: root3.destroy())
     root3.mainloop()
 
- #Método principal de lectura para rfid.
+#Método principal de lectura para rfid.
 def read_rfid():
     #Excepcion para que no deje de escuchar si no encuentra  lectura .
     try:
@@ -206,7 +207,6 @@ def read_rfid():
         ser = None
 
         #Serial instance defines where its going constantly listen , you must set the baudrate in accordance to your own module to process the entry. 9600 to rdm 6300, also set the dev path. These ones are defaults:
-
         try:
             ser = reader.Reader(0xffff, 0x0035, 84, 16, should_reset=False)
             ser.initialize()
@@ -216,9 +216,9 @@ def read_rfid():
         except:
             print "****ERROR ****"
 
-  #Lo que realiza este while es hacer que la instancia se ejecute siempre , entonces cada vez que
-  #se ejecuta un método de los implementados al principio de este script , luego de cerrar el navegador y la instancia de la aplicación gráfica de python
-  # volvería de nuevo a ejecutarse desde aquí permitiendo que se realize una y otra vez la verificacion de marcas RFID.
+ #Lo que realiza este while es hacer que la instancia se ejecute siempre , entonces cada vez que
+ #se ejecuta un método de los implementados al principio de este script , luego de cerrar el navegador y la instancia de la aplicación gráfica de python
+ # volvería de nuevo a ejecutarse desde aquí permitiendo que se realize una y otra vez la verificacion de marcas RFID.
         while True:
                 #Cada vez que se hace la lectura se realiza un flush para evitar que el buffer almacene algún dato erroneo
                 #almacenamos en la variable data la lectura realizada de RFID
@@ -240,32 +240,8 @@ def read_rfid():
     except:
        	 pass
 
-#Encargado de obtener el tipo de  usuario seleccionado y pasarlo a obtiene marca
-def obtieneTipoSeleccionado(dec, listBox):
-    listSeleccionado = listBox.curselection()
-    tipo = ""
-    for tem in listSeleccionado:
-        tipo = listBox.get(tem)
-
-
-    if tipo != "":
-        obtieneMarca(dec,tipo)
-    else:
-        root3 = Tk()
-        root3.attributes('-fullscreen', True)
-        frame3 = Frame(root3)
-        frame3.pack()
-        root3.config(background="black",cursor="none")
-
-        w2 = Label(root3, text="debbe seleccionar un tipo de usuario", wraplength=650,  fg = "red", bg = "black", font = "Helvetica 20 bold", height=70, width=100).pack()
-
-        root3.after(4000, lambda: root3.destroy())
-        root3.mainloop()
-
-
 #Obtiene la marca del usuario
 def obtieneMarca(dec,tipo):
-
     #Se crea el entorno gráfico  para realizar las marcas
     root = Tk()
     root.attributes('-fullscreen', True)
@@ -289,7 +265,6 @@ def obtieneMarca(dec,tipo):
     button4.place(x=50,y=240)
     button5.place(x=400,y=240)
     button6.place(x=50,y=350)
-
 
     #Se validan los botones de SIGUCA
     codigosExistentes=list(collection.find({"estado":"Activo"},{"tipo":  1,"codTarjeta": 1,"_id":1}))
@@ -345,11 +320,6 @@ def obtieneMarca(dec,tipo):
     root.after(10000, lambda: root.destroy())
     root.mainloop()
 
-
-
-
-
-
 #Se obtiene el tipo de usuario con el cual se debe hacer la marca
 def obtieneTipoUsuario(dec,listTipo):
         #Se crea el entorno gráfico  para realizar las marcas
@@ -357,28 +327,38 @@ def obtieneTipoUsuario(dec,listTipo):
         rootTipo.attributes('-fullscreen', True)
         rootTipo.config(height=600, width=600,cursor="none",bg="#ffffff")
 
-
-        lblTitle = Label(rootTipo,text="Seleccione un tipo de usuario",bd="2",bg= "#ffffff", fg="#55aa55", font="Helveltica 30 bold").place(x=150,y=2)
+        lblTitle = Label(rootTipo,text="Seleccione un tipo de usuario",bd="2",bg= "#ffffff", fg="#55aa55", font="Helveltica 30 bold").place(x=100,y=2)
 
         #Muestra los roles del usuario al cual le pertenece el llavin
         listBox = Listbox(rootTipo,bd="0",fg="#888888",font="Helveltica 30 bold",selectbackground="#00bb00",selectforeground="#ffffff",height=500,selectborderwidth=2, activestyle=NONE,highlightthickness=0,justify="center")
-        listBox.place(x=10,y=100)
+        listBox.place(x=25,y=100)
 
-        listBox.insert(0,*listTipo)
+	yAxis = 100
+	xAxis = 120
+	buttonList = list();
+	count = 1
+	for profile in listTipo:
+	    button = Button(rootTipo,text=str(profile), command = lambda: obtieneMarca(dec, profile),fg="white",activeforeground="white",activebackground="green",bg="green",width=20,height=4,bd=0,font="Helveltica 17 bold")
+	    button.place(x=xAxis,y=yAxis)
+	    buttonList.append(button)
+	    yAxis = yAxis + 110
+	    count = count + 1
+	    if count > 3:
+	        yAxis = 100
+		xAxis = 480
+		count = 1
 
+        listBox.insert(0,*buttonList)
 
-        buttonCancelar = Button(rootTipo,text="Cancelar",command=lambda: rootTipo.destroy(),fg="white",activeforeground="white",activebackground="red",bg="red",width=15,height=2,bd=6,font="Helveltica 17 bold").place(x=500,y=100)
+        buttonCancelar = Button(rootTipo,text="Cancelar",command=lambda: rootTipo.destroy(),fg="white",activeforeground="white",activebackground="red",bg="red",width=15,height=2,bd=6,font="Helveltica 17 bold").place(x=480,y=350)
 
-        buttonAceptar = Button(rootTipo,text="Aceptar",command= lambda: obtieneTipoSeleccionado(dec,listBox),fg="white",activeforeground="white",activebackground="#008800",bg="#00cc00",width=15,height=2,bd=6,font="Helveltica 17 bold").place(x=500,y=200)
-
+       # buttonAceptar = Button(rootTipo,text="Aceptar",command= lambda: obtieneTipoSeleccionado(dec,listBox),fg="white",activeforeground="white",activebackground="#008800",bg="#00cc00",width=15,height=2,bd=6,font="Helveltica 17 bold").place(x=500,y=200)
 
         rootTipo.after(5000, lambda: rootTipo.destroy())
         rootTipo.mainloop()
         return "Correcto"
 
-
 #En esta sección tenemos el orden de como se van a ir ejecutando los métodos dentro del sistema , esto es  lo que se ejecutará cuando se lance el script.
-
 while True:
     os.system('clear')
     dec=read_rfid()
@@ -418,7 +398,6 @@ while True:
                     #Se obtiene el tipo de usuario
                     tipoUsuario = obtieneTipoUsuario(dec,listTipo)
 
-        
-    else:  
+    else:
         os.system('clear')
-        pass 
+        pass
