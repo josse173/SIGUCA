9f5abef530deeef88de0bd0bf5fe7d591f3b66d8
rolando <rolando@costaricasoftwareservices.com>
2019-02-11 12:42:37 -0600

    RF-009 - Cambio para descontar de los periodos los días de vacaciones

--- routes/crudSolicitud.js
+++ routes/crudSolicitud.js
@@ -10,6 +10,7 @@ util 			= require('../util/util'),
 config          = require('../config.json'),
 emailSIGUCA 	= 'siguca@greencore.co.cr',
 HoraExtra		= require('../models/HoraExtra');
+PeriodoUsuario  = require('../models/PeriodoUsuario');
 
 exports.get = function(query, cb){
 	Solicitudes.find(query, function(error, solicitudes){
@@ -420,14 +421,34 @@ exports.deleteSoli = function(id, cb, idUser){
 //Gestionar Eventos
 //---------------------------------------------------------------------*/
 exports.gestionarSoli = function(solicitud, cb, idUser){
+
 	Usuario.findById(idUser, function (errUser, supervisor) {
 		Solicitudes.findByIdAndUpdate(solicitud.id,	{estado: solicitud.estado, comentarioSupervisor:solicitud.comentarioSupervisor}).populate('usuario').exec(function (err, soli) {
 			/*
 			 * Actualiza las vacaciones, solo cuando son aceptadas
 			 */
-			//if(solicitud.estado=='Aceptada' && solicitud.motivo == 'Vacaciones'){
-			//	Usuario.update({_id:soli.usuario}, {$inc:{vacaciones:(0-soli.cantidadDias)}},function(err){});
-			//}
+			if(solicitud.estado=='Aceptada' && solicitud.motivo == 'Vacaciones'){
+				PeriodoUsuario.find({usuario: soli.usuario._id}).sort({numeroPeriodo:1}).exec(function(error, periodos) {
+					if (error) return res.json(err);
+
+					var cantidadDias = soli.cantidadDias;
+
+					periodos.forEach(function (periodo) {
+						var diasDisponibles = periodo.diasAsignados - periodo.diasDisfrutados;
+						if(cantidadDias > 0 && cantidadDias <= diasDisponibles){
+							periodo.diasDisfrutados = periodo.diasDisfrutados + cantidadDias;
+							periodo.save(function (error, respuesta) {});
+							cantidadDias = 0;
+						} else {
+							if(cantidadDias > 0){
+								cantidadDias = cantidadDias - diasDisponibles;
+								periodo.diasDisfrutados = periodo.diasDisfrutados + diasDisponibles;
+								periodo.save(function (error, respuesta) {});
+							}
+						}
+					});
+				});
+			}
 
 			/*
 			 * Envía el correo electrónico
