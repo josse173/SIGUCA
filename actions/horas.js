f52ffbe75232b3c2b4bb3588fd361a58223ac826
rolando <rolando@costaricasoftwareservices.com>
2019-02-27 12:13:10 -0600

    Cambio en horas

--- actions/horas.js
+++ actions/horas.js
@@ -3,20 +3,16 @@ var Marca = require('../models/Marca');
 var util = require('../util/util');
 
 module.exports = {
-    horasTrabajadas: function (req, res) { 
+    horasTrabajadas: function (req, res) {
+        console.log(req.user);
+        console.log(req.session);
         var epochGte = moment();
         epochGte.hours(0);
         epochGte.minutes(0);
         epochGte.seconds(0);
-        //console.log(epochGte.unix());      
+        //console.log(epochGte.unix());
         var actualEpoch = moment();
-        Marca.find( 
-        {
-            usuario: req.user.id, 
-            epoch:{"$gte": epochGte.unix()}
-        },
-        {_id:0,tipoMarca:1,epoch:1}
-        ).exec(
+        Marca.find( { usuario: req.user.id, epoch:{"$gte": epochGte.unix()}, tipoMarca: "Entrada" , tipoUsuario: req.session.name}, {_id:0, tipoMarca:1, epoch:1} ).exec(
         function(error, arrayMarcas) {
             if (!error){
                 res.json(util.tiempoTotal(util.clasificarMarcas(arrayMarcas)));
