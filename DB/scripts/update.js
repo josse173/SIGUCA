2309f9a273bbd753e761125cd45e48b639afe79d
rolando <rolando@costaricasoftwareservices.com>
2020-03-27 10:24:33 -0600

    Agregar script de update de periodos

---
+++ DB/scripts/update.js
@@ -0,0 +1,2 @@
+db.periodousuarios.update({_id : ObjectId("5e5e90a78661ab176d066697")}, {$set : {nombrePeriodoPadre : "Rango 2", periodo : ObjectId("5cdc7d9c0a5a490b949657e4"), diasAsignados: 20}}, false, true);
+db.periodousuarios.update({_id : ObjectId("5e6fb6d248dfdb54a6a293bd")}, {$set : {nombrePeriodoPadre : "Rango 2", periodo : ObjectId("5cdc7d9c0a5a490b949657e4"), diasAsignados: 20}}, false, true);
